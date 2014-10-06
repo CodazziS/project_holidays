8b4c15ef8f955b137119be7179a51fcd7d280204
stephane- <tempo@codazzi.fr>
2014-10-06 10:58:41 +0200

    update

--- project_holidays.py
+++ project_holidays.py
@@ -7,11 +7,14 @@ class project_holidays_test(osv.Model):
 
     def _get_holidays(self, cr, uid, ids, field_name, arg, context):
 
-        d_start = datetime.date(2014, 1, 1)
+        d_start = datetime.date(2014, 1, 2)
         d_end = datetime.date(2015, 1, 1)
 
         res = []
-        x = self.pool.get('public.holidays.holidays').get_range(d_start, d_end)
+
+        x = self.pool.get('public.holidays.holidays')\
+            .get_range(cr, uid, d_start, d_end)
+        #x = self.pool.get('public.holidays.holidays').is_holiday(cr, uid, d_start)
         print x
         return res
 
